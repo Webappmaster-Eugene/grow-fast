0d3553989567ae5f6aac6cc8b7f862da4a24f4cf
Webappmaster-Eugene <johnn.hotmail@mail.ru>
2023-11-27 11:27:41 +0300

    feat: changes to RTK Query + Redux

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
 
@@ -17,14 +15,19 @@ export const Sidebar = ({ className }: SidebarProps) => {
     };
 
     return (
-        <div
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
-        >
-            <Button onClick={onToggle}>toggle</Button>
-            <div className={cls.switchers}>
-                <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
-            </div>
+      <div
+        data-testid="sidebar"
+        className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+          className,
+        ])}
+      >
+        <Button data-testid="si'ebar-toggle" o'Click={onToggle}>
+          toggle
+        </Button>
+        <div className={cls.switchers}>
+          <ThemeSwitcher />
+          <LangSwitcher className={cls.lang} />
         </div>
+      </div>
     );
 };
