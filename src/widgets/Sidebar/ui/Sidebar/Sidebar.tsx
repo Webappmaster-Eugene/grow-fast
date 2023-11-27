9f58da1c2a1dc36a0aee58e5ac49e50777030b12
Webappmaster-Eugene <johnn.hotmail@mail.ru>
2023-11-27 12:54:07 +0300

    fix: changes to RTK Query functionality

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,33 +1,36 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useState } from 'react';
-import { Button } from 'shared/ui/Button/Button';
-import cls from './Sidebar.module.scss';
+import { classNames } from "shared/lib/classNames/classNames";
+import { useState } from "react";
+import { Button } from "shared/ui/Button/Button";
+import cls from "./Sidebar.module.scss";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
-    className?: string;
+  className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-    const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
 
-    const onToggle = () => {
-        setCollapsed((prev) => !prev);
-    };
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
 
-    return (
-      <div
-        data-testid="sidebar"
-        className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
-          className,
-        ])}
-      >
-        <Button data-testid="si'ebar-toggle" o'Click={onToggle}>
-          toggle
-        </Button>
-        <div className={cls.switchers}>
-          <ThemeSwitcher />
-          <LangSwitcher className={cls.lang} />
-        </div>
+  return (
+    <div
+      data-testid="sidebar"
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        toggle
+      </Button>
+
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher className={cls.lang} />
       </div>
-    );
+    </div>
+  );
 };
